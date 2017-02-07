ec39d3f30e24f68d29554463bf1cf5280022f249
Sergio Morales <Sergio Morales@LAPTOP-R74K5D9H>
2017-02-07 03:14:39 -0600

    Commit

---
+++ ScorchetEarth/ArkClase4/JavaEscorchet.js
@@ -0,0 +1,12 @@
+﻿function Area_Cuadrado(num1) {
+    return parseInt(Math.pow(num1, 2))
+}
+function Area_Rectangulo(num1, num2) {
+    return parseInt(num1) * parseInt(num2)
+}
+function Area_Triangulo(num1, num2) {
+    return parseInt(parseInt(num1) * parseInt(num2) / parseInt(2))
+}
+function Area_Rombo(num1, num2) {
+    return parseInt(parseInt(num1) * parseInt(num2) / parseInt(2))
+}
